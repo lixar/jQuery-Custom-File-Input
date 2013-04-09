7fdac13c7853f6cf732dfd43bc474e37472344b6
gcatellier <gcatellier@lixar.com>
2013-04-09 17:25:19 -0300

    Update jQuery.fileinput.js
    Converted event binding chain to use .bind()'s event-map method.

--- js/jQuery.fileinput.js
+++ js/jQuery.fileinput.js
@@ -12,35 +12,40 @@
 		var options = $.extend({}, $.fn.customFileInput.defaults, options);
 		
 		//apply events and styles for file input element
-		var fileInput = $(this).addClass('customfile-input') //add class for CSS
-		.bind('disable', function() {
-			fileInput.attr('disabled', true);
-			upload.addClass('customfile-disabled');
-		}).bind('enable', function() {
-			fileInput.removeAttr('disabled');
-			upload.removeClass('customfile-disabled');
-		}).bind('checkChange', function() {
-			if (fileInput.val() && fileInput.val() != fileInput.data('val')) {
-				fileInput.trigger('change');
+		var fileInput = $(this).addClass('customfile-input').bind({
+			disable: function(){
+				fileInput.attr('disabled', true);
+				upload.addClass('customfile-disabled');
+			},
+			enable: function(){
+				fileInput.removeAttr('disabled');
+				upload.removeClass('customfile-disabled');
+			},
+			checkChange: function(){
+				if (fileInput.val() && fileInput.val() != fileInput.data('val')) {
+					fileInput.trigger('change');
+				}
+			},
+			change: function(){
+				//get file name
+				var fileName = $(this).val().split(/\\/).pop();
+				//get file extension
+				var fileExt = 'customfile-ext-' + fileName.split('.').pop().toLowerCase();
+				//update the feedback
+				uploadFeedback.text(fileName) //set feedback text to filename
+				.removeClass(uploadFeedback.data('fileExt') || '') //remove any existing file extension class
+				.addClass(fileExt) //add file extension class
+				.data('fileExt', fileExt) //store file extension for class removal on next change
+				.addClass('customfile-feedback-populated'); //add class to show populated state
+				//change text of button	
+				uploadButton.text('Change');
+			},
+			click: function(){
+				fileInput.data('val', fileInput.val());
+				setTimeout(function() {
+					fileInput.trigger('checkChange');
+				}, 100);
 			}
-		}).bind('change', function() {
-			//get file name
-			var fileName = $(this).val().split(/\\/).pop();
-			//get file extension
-			var fileExt = 'customfile-ext-' + fileName.split('.').pop().toLowerCase();
-			//update the feedback
-			uploadFeedback.text(fileName) //set feedback text to filename
-			.removeClass(uploadFeedback.data('fileExt') || '') //remove any existing file extension class
-			.addClass(fileExt) //add file extension class
-			.data('fileExt', fileExt) //store file extension for class removal on next change
-			.addClass('customfile-feedback-populated'); //add class to show populated state
-			//change text of button	
-			uploadButton.text('Change');
-		}).click(function() { //for IE and Opera, make sure change fires after choosing a file, using an async callback
-			fileInput.data('val', fileInput.val());
-			setTimeout(function() {
-				fileInput.trigger('checkChange');
-			}, 100);
 		});
 	
 		//create custom control container
